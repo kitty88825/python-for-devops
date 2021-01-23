a252a3bdc16e2e7b8fc7812f40ac3628973d710d
kitty88825 <kitty40632@gmail.com>
2021-01-23 19:15:45 +0800

    Add simple_click.py

---
+++ simple_click.py
@@ -0,0 +1,17 @@
+'''
+Need pip install click
+
+Simple Click example
+'''
+import click
+
+
+@click.command()
+@click.option('--greeting', default='Hiya', help='How do you want to greet?')
+@click.option('--name', default='Tammy', help='Who do you want to greet?')
+def greet(greeting, name):
+    print(f'{greeting} {name}')
+
+
+if __name__ == '__main__':
+    greet()
