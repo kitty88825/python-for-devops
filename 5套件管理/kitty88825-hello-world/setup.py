30401011bbb8181e60cd70b79be564a4adecaf5c
kitty88825 <kitty40632@gmail.com>
2021-01-29 20:59:25 +0800

    Add kitty88825-hello-world files

---
+++ 5套件管理/kitty88825-hello-world/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kitty88825-hello-world",
+    version="0.0.1",
+    author="XinCenXie",
+    author_email="kitty40632@gmail.com",
+    url="https://github.com/kitty88825/python-for-devops.git",
+    description="A hello-world example package to the Python for devops.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
